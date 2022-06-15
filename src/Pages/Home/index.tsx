63109381c932fba7f49aa9289663aa6eb1a57c02
Abdelrahman Abdallah <abdelrahman202022@gmail.com>
2022-06-16 00:23:16 +0200

    add inital structure

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import { Container } from "react-bootstrap";
+
+const Home: FC = () => {
+  return (
+    <Container>
+      <h1>hello world</h1>
+    </Container>
+  );
+};
+
+export default Home;
